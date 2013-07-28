58da9aa656aa14341e21436b36acccfbe6150195
Tim Smith <smith@ttimsmith.com>
2013-07-28 13:05:07 -0500

    Registering Custom Post type, and removing unnecessary menus from Admin

--- themes/babblenut_v1/functions.php
+++ themes/babblenut_v1/functions.php
@@ -4,7 +4,7 @@
 if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
    wp_deregister_script('jquery');
-   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js", false, null);
+   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js", false, null);
    wp_enqueue_script('jquery');
 }
 
@@ -12,44 +12,59 @@ function my_jquery_enqueue() {
 wp_register_script( 'global-stuff', get_template_directory_uri() . '/assets/js/stuff-ck.js');
 wp_enqueue_script( 'global-stuff', 'true', 'true', 'true', 'true' );
 
-// Register Custom Post Type
-// add_action( 'init', 'create_post_type');
-// function create_post_type() {
-//   register_post_type( 'episodes',
-//     array(
-//       'labels' => array(
-//         'name' => 'Episodes',
-//         'singular_name' => 'Episode',
-//         'menu_name' => 'Episodes',
-//         'add_new' => 'Add Episode',
-//         'add_new_item' => 'Add New Episode',
-//         'edit' => 'Edit',
-//         'edit_item' => 'Edit Episode',
-//         'new_item' => 'New Episode',
-//         'view' => 'View Episode',
-//         'view_item' => 'View Episode',
-//         'search_items' => 'Search Episodes',
-//         'not_found' => 'No Episodes Found',
-//         'not_found_in_trash' => 'No Episodes Found in Trash',
-//         'parent' => 'Parent Episode',
-//       ),
-//     'public' => true,
-//     'show_ui' => true,
-//     'show_in_menu' => true,
-//     'show_in_nav_menus' => true,
-//     'show_in_admin_bar' => true,
-//     'menu_position' => 5,
-//     'capability_type' => 'post',
-//     'hierarchical' => false,
-//     'rewrite' => array('slug' => 'ep', 'with_front' => '' ),
-//     'query_var' => true,
-//     'exclude_from_search' => false,
-//     'has_archive' => true,
-//     'supports' => array( 'title','editor','excerpt','custom-fields','thumbnail', 'revisions' ),
-//     'taxonomies' => array( 'category' ),
-//     )
-//   );
-// }
+// Episodes Post Type
+
+add_action( 'init', 'create_post_type');
+function create_post_type() {
+  register_post_type( 'episodes',
+    array(
+      'labels' => array(
+        'name' => 'Episodes',
+        'singular_name' => 'Episode',
+        'menu_name' => 'Episodes',
+        'add_new' => 'Add Episode',
+        'add_new_item' => 'Add New Episode',
+        'edit' => 'Edit',
+        'edit_item' => 'Edit Episode',
+        'new_item' => 'New Episode',
+        'view' => 'View Episode',
+        'view_item' => 'View Episode',
+        'search_items' => 'Search Episodes',
+        'not_found' => 'No Episodes Found',
+        'not_found_in_trash' => 'No Episodes Found in Trash',
+        'parent' => 'Parent Episode',
+      ),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'show_in_nav_menus' => true,
+    'show_in_admin_bar' => true,
+    'menu_position' => 5,
+    'capability_type' => 'post',
+    'hierarchical' => false,
+    'rewrite' => array('slug' => 'ep', 'with_front' => '' ),
+    'query_var' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'supports' => array( 'title','editor','excerpt','custom-fields','thumbnail', 'revisions' ),
+    'taxonomies' => array( 'category' ),
+    )
+  );
+}
+
+// Removing Unnecessary Stuff from Admin Menu
+
+function remove_menus () {
+global $menu;
+  $restricted = array(__('Posts'), __('Media'), __('Links'),__('Comments'));
+  end ($menu);
+  while (prev($menu)){
+    $value = explode(' ',$menu[key($menu)][0]);
+    if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+  }
+}
+add_action('admin_menu', 'remove_menus');
+
 
 // Messing with WP Admin Bar
 function mytheme_admin_bar_render() {
@@ -61,6 +76,7 @@ function mytheme_admin_bar_render() {
     $wp_admin_bar->remove_menu('new-link', 'new-content');
     $wp_admin_bar->remove_menu('new-media', 'new-content');
     $wp_admin_bar->remove_menu('new-user', 'new-content');
+    $wp_admin_bar->remove_menu('new-post', 'new-content');
 }
 // and we hook our function via
 add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
