a3957a4b44c15469426b06f8f022a8f529102add
Tim Smith <smith@ttimsmith.com>
2013-07-29 00:42:33 -0500

    Single Episode Page

--- themes/babblenut_v1/single.php
+++ themes/babblenut_v1/single.php
@@ -1,4 +1,39 @@
 <?php get_header(); ?>
 
+<main class="site__content" role="main">
+	
+	<?php get_sidebar('single'); ?>
+
+	<section class="content__main-column">
+		
+		<?php if (have_posts()) : the_post(); ?>
+			
+			<article class="entry">
+				<h2>#<?php the_field('episode_number');?>: <?php the_title(); ?></h2>
+				<p class="pubdate">
+					<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time> &middot; 
+					<?php if( get_field('episode_hours')) : ?><?php the_field('episode_hours'); ?> hour<?php endif; ?> <?php the_field('episode_minutes'); ?> minutes
+				</p>
+
+				<p><?php the_field('episode_lead'); ?></p>
+
+				<?php if(get_field('episode_sponsor')) : ?>
+					<div class="entry__sponsor">
+						<p><strong>This week’s Babblenut is brought to you by:</strong></p>
+						<p><?php the_field('episode_sponsor'); ?></p>
+					</div>
+					<hr>
+				<?php endif; ?>
+				
+						
+				<?php the_content(); ?>
+
+			</article>
+
+		<?php endif; ?>
+
+	</section><!-- .content__main-column -->
+</main><!-- .site__content -->
+
 
 <?php get_footer(); ?>
