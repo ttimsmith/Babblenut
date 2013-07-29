eb2ddde80d13163390bbc72b1f9c2af64ca7e2f9
Tim Smith <smith@ttimsmith.com>
2013-07-29 00:38:38 -0500

    Adding Host Info to Sidebar

--- themes/babblenut_v1/sidebar.php
+++ themes/babblenut_v1/sidebar.php
@@ -15,7 +15,7 @@
 				</a></li>
 				<li><a href="/sponsorship">
 					<span data-icon="h" aria-hidden="true"></span>
-					<span>Sponsor Bytesize</span>
+					<span>Sponsor Babblenut</span>
 				</a></li>
 			</ul><!-- .useful-links -->		
 		</div><!-- .info__module -->
@@ -24,18 +24,18 @@
 			<h4>Hosted By</h4>
 			<div class="module__host">
 				<figure class="host__image">
-					<img src="http://placehold.it/100" alt="Paul Armstrong">
+					<img src="<?php bloginfo('template_url'); ?>/assets/img/paul_100.png" alt="Paul Armstrong">
 				</figure>
-				<h5>Paul Armstrong</h5>
+				<h5><a href="http://paularmstrong.me">Paul Armstrong</a></h5>
 				<p><a href="https://twitter.com/wiseacre" class="twitter-follow-button" data-show-count="false" data-dnt="true">Follow @wiseacre</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></p>
 			</div> <!-- .module__host -->
 
 			<div class="module__host">
 				<figure class="host__image">
-					<img src="http://placehold.it/100" alt="Paul Armstrong">
+					<img src="<?php bloginfo('template_url'); ?>/assets/img/tim_100.png" alt="Paul Armstrong">
 				</figure>
-				<h5>Tim Smith</h5>
+				<h5><a href="http://ttimsmith.com">Tim Smith</a></h5>
 				<p><a href="https://twitter.com/ttimsmith" class="twitter-follow-button" data-show-count="false" data-dnt="true">Follow @ttimsmith</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></p>
 			</div><!-- .module__host -->
