05ca923377ecabc176ea401a39a68ff4a3df4693
Tim Smith <smith@ttimsmith.com>
2013-07-30 18:34:45 -0500

    Adding a default page layout

--- themes/babblenut_v1/page.php
+++ themes/babblenut_v1/page.php
@@ -1,4 +1,21 @@
 <?php get_header(); ?>
 
+	<main class="site__content" role="main">
+		
+		<?php get_sidebar(); ?>
+
+		<section class="content__main-column">
+			<?php the_post(); ?>
+
+			<div class="entry">
+				<h1 class="page-title"><?php the_title(); ?></h1>
+
+				<?php the_content(); ?>
+	      
+			</div><!-- .entry -->
+
+		</section><!-- .content__main-column -->
+	</main><!-- .site__content -->
+
 
 <?php get_footer(); ?>
