20c1a4d77c7402c8788feda8a410348447e01398
Tim Smith <smith@ttimsmith.com>
2013-07-30 20:00:13 -0500

    Commented out iTunes link

--- themes/babblenut_v1/footer.php
+++ themes/babblenut_v1/footer.php
@@ -1,6 +1,6 @@
 
 <footer class="site__footer" role="contentinfo">
-	<small><a href="#">Subscribe via iTunes</a> &middot; <a href="/feed/babblenut/">Subscribe via RSS</a></small>
+	<small><!-- <a href="#">Subscribe via iTunes</a> &middot; --> <a href="/feed/babblenut/">Subscribe via RSS</a></small>
 	<small><em>Babblenut</em> &copy; <?php echo date('Y'); ?> by <a href="http://paularmstrong.me">Paul Armstrong</a> &amp; <a href="http://ttimsmith.com">Tim Smith</a>.</small>
 </footer>
 
